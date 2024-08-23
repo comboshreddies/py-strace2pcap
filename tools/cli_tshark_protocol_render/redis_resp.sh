ff3de74ab5f9da40cf26ebcb8fef08142d004fa2
none <loopback@null.net>
2024-08-23 16:52:30 +0200

    adding few cli terminal tshark pcap rendering scripts

---
+++ tools/cli_tshark_protocol_render/redis_resp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+tshark -r $1 -T pdml -Y "tcp.port==$2"  |\
+  grep   -e 'frame.time_epoch' -e 'tcp.payload' |\
+  sed 's/.*"http.request.uri".* show="\(.*\)" value=.*/{"q":"\1"},/g' |\
+  sed 's/.*frame.time_epoch.*show="\([0123456789.]*\)".*/{"time": "\1"},/g' |\
+  sed 's|.* value="\(.*\)"/>|\1,\n\r|g' | \
+  awk '{if($1 ~ /{/) print $0 ; else {print "" ;system("echo "$0" | xxd -r -p ")}}'
